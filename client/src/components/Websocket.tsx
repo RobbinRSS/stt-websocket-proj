5eb8bae845c30068641a51d0388a76ddeae07519
Robbin <robbin@desktop-kj0mlh0.home>
2026-07-31 17:14:13 +0200

    created a message payload to see the history and an emit from client to server which will recieve the message back from server to client

--- client/src/components/Websocket.tsx
+++ client/src/components/Websocket.tsx
@@ -1,8 +1,16 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { WebsocketContext } from "../contexts/WebsocketContext";
+
+//custom type
+type MessagePayload = {
+    content: string;
+    msg: string;
+}
 
 export const Websocket = () => {
 
+    const [value, setValue] = useState('');
+    const [messages, setMessages] = useState<MessagePayload[]>([])
     // server socket
     const socket = useContext(WebsocketContext);
 
@@ -11,9 +19,10 @@ export const Websocket = () => {
         socket.on("connect", () => {
             console.log("connected");
         })
-        socket.on("onMessage", (data) => {
+        socket.on("onMessage", (message: MessagePayload) => {
             console.log("onMessage event received");
-            console.log(data);
+            console.log(message);
+            setMessages((prev) => [...prev, message])
         });
 
         // when component unmounts unregister those events
@@ -24,10 +33,22 @@ export const Websocket = () => {
         }
     }, []);
 
+    const onSubmit = () => {
+        socket.emit('newMessage', value);
+        setValue('');
+    }
+
     return (
         <div>
             <div>
                 <h1>Websocket Component</h1>
+                <div>
+                    {messages.length === 0 ? (<div>No Messages</div>) : (<div>{messages.map((msg) => (<div><p>{msg.content}</p></div>))}</div>)}
+                </div>
+                <div>
+                    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+                    <button onClick={onSubmit}>Submit</button>
+                </div>
             </div>
         </div>
     );
